5403a631fa27012ce3aefdd3716d8ca7b7ad4a46
Sandeep Gupta <sangupta@adobe.com>
2015-05-25 15:38:18 +0530

    Remove the authKey as hard-coded

--- src/main/java/com/sangupta/diggdump/DiggDumpCommand.java
+++ src/main/java/com/sangupta/diggdump/DiggDumpCommand.java
@@ -15,7 +15,7 @@ public abstract class DiggDumpCommand implements Runnable {
 			throw new IllegalArgumentException("WebRequest cannot be empty/null");
 		}
 
-		request.addHeader("Cookie", "__utma=146621099.1988356693.1410165955.1411728330.1411985056.17; twitter-news-optout=1; __qca=P0-33966875-1428897818024; _xsrf=0bed6831c1664469bbb3507152c19ab5; _chartbeat_uuniq=3; _cb_ls=1; preferred_view=desktop; _ga=GA1.2.1988356693.1410165955; frontend.user=\"eyJ1c2VyX2lkIjogIjAzYzdiMThmODhmYzQ4OTk5OTNjOTQ2ZGJiNmY2NDk5IiwgImdyb3VwcyI6IFsicmVhZGVyIl0sICJ2IjogIjEuNi4wIn0=|1432541180|1f1c4a6b6be4c8984ae8df7edd19131cdff4b4ec\"; _chartbeat2=3vT9iDzJR8UDublbn.1410164084683.1432541178892.0111110011101001");
+		request.addHeader("Cookie", this.authKey);
 		request.addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.65 Safari/537.36");
 	}
 }
